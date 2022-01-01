39ba1764df43cdfab188dbb01c0caf7d4c5c94fe
Nittymol <68943565+Nittymol@users.noreply.github.com>
2022-01-01 11:19:31 +0530

    Create app1.js

---
+++ app1.js
@@ -0,0 +1,10 @@
+// Assignment-1 --
+// Q1. Write a program to check whether the number is greater than 10 or not
+var a = prompt("Enter the no.")
+a = parseInt(a);
+if(a>10){
+console.log("Number is greater than 10")
+}
+else{
+console.log("Number is not greater than 10")
+}
