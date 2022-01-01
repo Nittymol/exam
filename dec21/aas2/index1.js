9b87b08ad20e93147d34f5c04b73de59fa5c766b
Nittymol <68943565+Nittymol@users.noreply.github.com>
2022-01-01 11:46:01 +0530

    Create index1.js

---
+++ dec21/aas2/index1.js
@@ -0,0 +1,15 @@
+var char = "d";
+
+if(char == "a" || char =="A") {
+ console.log("It is a vowel");
+} else if(char == "E" || char == "E"){
+ console.log("It is a vowel");
+} else if(char == "i" || char =="I") {
+ console.log("It is a vowel");
+} else if(char == "o" || char =="O") {
+ console.log("It is a vowel");
+} else if(char == "u" || char =="U") {
+ console.log("It is a vowel");
+} else {
+ console.log("It is a consonant");
+}
